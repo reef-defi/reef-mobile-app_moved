022d6e4e0eb81151b112d8706c3d7727806f04b7
Boidushya <boidushyabhattacharya@gmail.com>
2022-07-25 01:12:37 +0530

    Splash screen (#21)
    * feat: added splash screen functionality
    
    * feat: added splash screen functionality

--- lib/pages/SplashScreen.dart
+++ lib/pages/SplashScreen.dart
@@ -2,6 +2,9 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:gap/gap.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:reef_mobile_app/utils/styles.dart';
 import '../main.dart';
 import '../model/ReefState.dart';
 import '../service/JsApiService.dart';
@@ -24,6 +27,7 @@ class SplashApp extends StatefulWidget {
 
 class _SplashAppState extends State<SplashApp> {
   bool _hasError = false;
+  bool _isGifFinished = false;
   Widget? onInitWidget;
   var displayContent = false;
 
@@ -31,6 +35,11 @@ class _SplashAppState extends State<SplashApp> {
   void initState() {
     super.initState();
     _initializeAsyncDependencies();
+    Timer(const Duration(milliseconds: 3830), () {
+      setState(() {
+        _isGifFinished = true;
+      });
+    });
   }
 
   Future<void> _initializeAsyncDependencies() async {
@@ -63,10 +72,59 @@ class _SplashAppState extends State<SplashApp> {
       );
     }
 
+    //TODO: Initialise the widget back
     return Stack(children: <Widget>[
       widget.reefJsApiService.widget,
       displayContent == false
-          ? Center(child: CircularProgressIndicator())
+          ? Stack(
+              children: [
+                Container(
+                  width: double.infinity,
+                  color: Styles.splashBackgroundColor,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.asset(
+                        "assets/images/intro.gif",
+                        height: 128.0,
+                        width: 128.0,
+                      ),
+                    ],
+                  ),
+                ),
+                Positioned(
+                  bottom: 24,
+                  right: 24,
+                  child: AnimatedOpacity(
+                    duration: const Duration(milliseconds: 250),
+                    curve: Curves.easeInOutCirc,
+                    opacity: _isGifFinished ? 1 : 0,
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text(
+                          "Loading App",
+                          style: GoogleFonts.lato(
+                              fontWeight: FontWeight.w400,
+                              fontSize: 16,
+                              color: Styles.textLightColor,
+                              decoration: TextDecoration.none),
+                        ),
+                        const Gap(4),
+                        SizedBox(
+                          height: 12,
+                          width: 12,
+                          child: CircularProgressIndicator.adaptive(
+                              strokeWidth: 2,
+                              valueColor: AlwaysStoppedAnimation<Color>(
+                                  Styles.textLightColor)),
+                        ),
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            )
           : widget.displayOnInit(),
     ]);
   }
