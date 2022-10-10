43366afdb5668d11bf67a641f50c1c6614f3ab67
boo-0x <boo_0x@tutanota.com>
2022-10-10 13:45:48 +0200

    Bind EVM modal complete

--- lib/components/modals/bind_modal.dart
+++ lib/components/modals/bind_modal.dart
@@ -4,6 +4,8 @@ import 'package:gap/gap.dart';
 import 'package:reef_mobile_app/components/modal.dart';
 import 'package:reef_mobile_app/model/ReefAppState.dart';
 import 'package:reef_mobile_app/model/account/ReefSigner.dart';
+import 'package:reef_mobile_app/model/tokens/TokenWithAmount.dart';
+import 'package:reef_mobile_app/utils/constants.dart';
 import 'package:reef_mobile_app/utils/elements.dart';
 import 'package:reef_mobile_app/utils/styles.dart';
 import 'package:reef_mobile_app/utils/functions.dart';
@@ -21,28 +23,47 @@ class BindEvm extends StatefulWidget {
 }
 
 class _BindEvmState extends State<BindEvm> {
-  TextEditingController valueContainer = TextEditingController();
-  bool value = false;
+  bool selectAccount = false;
+  dynamic transferBalanceFrom;
+  List<ReefSigner> availableTxAccounts = [];
 
-  hasBalanceForBinding(ReefSigner reefSigner) {
+  @override
+  void initState() {
+    super.initState();
+
+    if (hasBalanceForBinding(widget.bindFor)) return;
+
+    availableTxAccounts = getSignersWithEnoughBalance(widget.bindFor);
+    if (availableTxAccounts.isNotEmpty) {
+      transferBalanceFrom = availableTxAccounts[0];
+    }
+  }
+
+  bool hasBalanceForBinding(ReefSigner reefSigner) {
     return reefSigner.balance >= BigInt.from(MIN_BALANCE * 1e18);
   }
 
-  getSignersWithEnoughBalance(ReefSigner bindFor) {
-    List<ReefSigner> availableTxAccounts = ReefAppState
-        .instance.model.accounts.signers
-        .where((signer) =>
-            signer.address != bindFor.address && hasBalanceForBinding(signer))
-        .toList();
-    availableTxAccounts.sort((a, b) => a.balance.compareTo(b.balance));
-    return availableTxAccounts;
+  bool hasBalanceForFunding(ReefSigner reefSigner) {
+    return reefSigner.balance >= BigInt.from(MIN_BALANCE * 1e18 * 2);
   }
 
-  bind(String address) async {
-    var response =
-        await ReefAppState.instance.accountCtrl.bindEvmAccount(address);
-    // TODO wait for tx to be included in block ?
-    print(response);
+  List<ReefSigner> getSignersWithEnoughBalance(ReefSigner bindFor) {
+    List<ReefSigner> _availableTxAccounts = ReefAppState
+        .instance.model.accounts.signers
+        .where((signer) =>
+            signer.address != bindFor.address && hasBalanceForFunding(signer))
+        .toList();
+    _availableTxAccounts.sort((a, b) => b.balance.compareTo(a.balance));
+    return _availableTxAccounts;
+  }
+
+  transfer() async {
+    TokenWithAmount? reefToken = ReefAppState.instance.model.tokens.tokenList
+        .firstWhere((token) => token.address == Constants.REEF_TOKEN_ADDRESS);
+    reefToken = reefToken.setAmount(BigInt.from(MIN_BALANCE * 1e18).toString());
+
+    await ReefAppState.instance.transferCtrl.transferTokens(
+        transferBalanceFrom.address, widget.bindFor.address, reefToken);
   }
 
   Widget buildAccount(ReefSigner signer) {
@@ -103,6 +124,124 @@ class _BindEvmState extends State<BindEvm> {
     );
   }
 
+  Widget buildButton(Function func) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(12),
+      ),
+      width: double.infinity,
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(40)),
+          shadowColor: const Color(0x559d6cff),
+          elevation: 5,
+          backgroundColor: Styles.primaryAccentColorDark,
+          padding: const EdgeInsets.symmetric(vertical: 16),
+        ),
+        onPressed: () {
+          func();
+        },
+        child: const Text(
+          'Continue',
+          style: TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.w700,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget buildSelectAccount() {
+    return Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+      const Text("Select account",
+          style: TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.w700,
+          )),
+      const Gap(8),
+      ...availableTxAccounts
+          .map<Widget>((account) => Column(children: [
+                InkWell(
+                    onTap: () {
+                      setState(() => {
+                            transferBalanceFrom = account,
+                            selectAccount = false
+                          });
+                    },
+                    child: (buildAccount(account))),
+                const Gap(8),
+              ]))
+          .toList(),
+      const Gap(8),
+    ]);
+  }
+
+  Widget buildBound() {
+    return Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+      const Text("Successfully connected to Ethereum VM address "),
+      Text(widget.bindFor.evmAddress.shorten(),
+          style: const TextStyle(fontWeight: FontWeight.bold)),
+      const Gap(48),
+    ]);
+  }
+
+  Widget buildBind() {
+    return Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+      Column(crossAxisAlignment: CrossAxisAlignment.start, children: const [
+        Text("Start using Reef EVM smart contracts."),
+        Gap(8),
+        Text("First connect EVM address for:"),
+        Gap(8)
+      ]),
+      // Bind for
+      buildAccount(widget.bindFor),
+      const Gap(16),
+      if (hasBalanceForBinding(widget.bindFor)) ...[
+        // Bind button
+        buildButton(() {
+          ReefAppState.instance.accountCtrl
+              .bindEvmAccount(widget.bindFor.address);
+          Navigator.pop(context);
+        })
+      ] else ...[
+        // Unsufficient balance
+        if (transferBalanceFrom == null) ...[
+          // No other accounts with enough balance
+          const Text(
+              "Not enough REEF in account for connect EVM address transaction fee."),
+          const Gap(16),
+        ] else ...[
+          // Other accounts with enough balance
+          Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+            Row(children: const [
+              Text("~$MIN_BALANCE REEF ",
+                  style: TextStyle(fontWeight: FontWeight.bold)),
+              Text("is needed for transaction fee."),
+            ]),
+            const Gap(24),
+            const Text("Coins will be transfered from account:"),
+            const Gap(8)
+          ]),
+          // Transfer from
+          InkWell(
+              onTap: () {
+                if (availableTxAccounts.length > 1) {
+                  setState(() => selectAccount = true);
+                }
+              },
+              child: (buildAccount(transferBalanceFrom))),
+          const Gap(16),
+          buildButton(() {
+            transfer();
+            Navigator.pop(context);
+          }),
+        ],
+      ]
+    ]);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -110,70 +249,12 @@ class _BindEvmState extends State<BindEvm> {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          if (!widget.bindFor.isEvmClaimed) ...[
-            Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: const [
-                  Text("Start using Reef EVM smart contracts."),
-                  Gap(8),
-                  Text("First connect EVM address for:"),
-                  Gap(8)
-                ]),
-            // Bind for
-            buildAccount(widget.bindFor),
-            const Gap(16),
-            // Unsufficient balance
-            Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-              Row(children: const [
-                Text("~$MIN_BALANCE REEF ",
-                    style: TextStyle(fontWeight: FontWeight.bold)),
-                Text("is needed for transaction fee."),
-              ]),
-              const Gap(24),
-              const Text("Coins will be transfered from account:"),
-              const Gap(8)
-            ]),
-            // Transfer from
-            buildAccount(widget.bindFor),
-            const Gap(16),
-          ] else ...[
-            // Already bound
-            Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-              const Text("Successfully connected to Ethereum VM address "),
-              Text(widget.bindFor.evmAddress.shorten(),
-                  style: const TextStyle(fontWeight: FontWeight.bold)),
-              const Gap(48),
-            ]),
-          ],
-          // Continue button
-          Container(
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(12),
-            ),
-            width: double.infinity,
-            child: ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(40)),
-                shadowColor: const Color(0x559d6cff),
-                elevation: 5,
-                backgroundColor: Styles.primaryAccentColorDark,
-                padding: const EdgeInsets.symmetric(vertical: 16),
-              ),
-              onPressed: () async {
-                await widget.callback();
-                if (!mounted) return;
-                Navigator.pop(context);
-              },
-              child: const Text(
-                'Continue',
-                style: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.w700,
-                ),
-              ),
-            ),
-          ),
+          if (selectAccount)
+            buildSelectAccount()
+          else if (widget.bindFor.isEvmClaimed)
+            buildBound()
+          else
+            buildBind(),
         ],
       ),
     );
