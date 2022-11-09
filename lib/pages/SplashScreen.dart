06305493e4fd99cf01ff0de7d96088d12a701d28
boo-0x <boo_0x@tutanota.com>
2022-11-09 10:04:10 +0100

    Open biometrics auth automatically if available

--- lib/pages/SplashScreen.dart
+++ lib/pages/SplashScreen.dart
@@ -74,6 +74,7 @@ class _SplashAppState extends State<SplashApp> {
       });
     });
     _checkBiometricsSupport().then((value) {
+      if (value) authenticateWithBiometrics();
       setState(() {
         _biometricsIsAvailable = value;
       });
@@ -94,11 +95,10 @@ class _SplashAppState extends State<SplashApp> {
       title: 'Reef Chain App',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-        textTheme: GoogleFonts.poppinsTextTheme(
-          Theme.of(context).textTheme,
-        )
-      ),
+          primarySwatch: Colors.blue,
+          textTheme: GoogleFonts.poppinsTextTheme(
+            Theme.of(context).textTheme,
+          )),
       home: _buildBody(),
       navigatorKey: navigatorKey,
     );
