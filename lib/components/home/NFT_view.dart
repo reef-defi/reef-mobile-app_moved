c262182467e036b7098c027f0dd893857c3abb82
Matjaz Hirsman <matjazonline@gmail.com>
2022-11-13 18:41:25 +0100

    nft card

--- lib/components/home/NFT_view.dart
+++ lib/components/home/NFT_view.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:reef_mobile_app/model/tokens/TokenNFT.dart';
 import 'package:reef_mobile_app/utils/elements.dart';
 import 'package:reef_mobile_app/utils/styles.dart';
+
+import '../../model/ReefAppState.dart';
 
 class NFTView extends StatefulWidget {
   const NFTView({Key? key}) : super(key: key);
@@ -9,7 +13,66 @@ class NFTView extends StatefulWidget {
   State<NFTView> createState() => _NFTViewState();
 }
 
+
 class _NFTViewState extends State<NFTView> {
+  Widget tokenCard(String name,
+      {String iconURL,
+        double balance = 0.0}) {
+    return ViewBoxContainer(
+        child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 24.0),
+            child: Column(
+              children: [
+                SizedBox(
+                    height: 48,
+                    width: 48,
+                    child: CachedNetworkImage(
+                      imageUrl: iconURL,
+                      placeholder: (context, url) => Shimmer.fromColors(
+                        baseColor: Colors.grey[300]!,
+                        highlightColor: Colors.grey[350]!,
+                        child: Container(
+                          width: 48,
+                          height: 48,
+                          decoration: ShapeDecoration(
+                            color: Colors.grey[350]!,
+                            shape: const CircleBorder(),
+                          ),
+                        ),
+                      ),
+                      errorWidget: (context, url, error) => const Icon(
+                        CupertinoIcons.exclamationmark_circle_fill,
+                        color: Colors.black12,
+                        size: 48,
+                      ),
+                    ),
+                const Gap(8),
+                Text(
+                  name,
+                  style: TextStyle(
+                    color: Styles.textColor,
+                    fontSize: 20,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+                const Gap(2),
+                Text(
+                  // TODO allow conversionRate to be null for no data
+                  "${balance != 0 ? balance : 0} ${tokenName != "" ? tokenName : name.toUpperCase()} - ${price != 0 ? price.toStringAsFixed(4) : 'No pool data'}",
+                  style: TextStyle(color: Styles.textLightColor, fontSize: 16),
+                ),
+                const Gap(8),
+                if (balance != 0)
+                  GradientText(
+                    "\$${getBalanceValue(balance, price).toStringAsFixed(2)}",
+                    style: GoogleFonts.spaceGrotesk(
+                        fontSize: 24, fontWeight: FontWeight.w700),
+                    gradient: textGradient(),
+                  ),
+              ],
+            )));
+  }
+
   @override
   Widget build(BuildContext context) {
     return ListView(
@@ -17,28 +80,29 @@ class _NFTViewState extends State<NFTView> {
         padding: const EdgeInsets.all(0),
         children: [
           SizedBox(
-            // constraints: const BoxConstraints.expand(),
             width: double.infinity,
-            // // replace later, just for debugging
-            // decoration: BoxDecoration(
-            //   border: Border.all(
-            //     color: Colors.red,
-            //   ),
-            // ),
             child: Padding(
               padding:
-                  const EdgeInsets.symmetric(vertical: 32, horizontal: 48.0),
-              child: ViewBoxContainer(
-                  child: Padding(
-                padding: const EdgeInsets.symmetric(vertical: 24.0),
-                child: Column(
-                  children: [
-                    Text("No NFTs in your wallet",
-                        style: TextStyle(
-                            color: Styles.textLightColor, fontSize: 16)),
-                  ],
-                ),
-              )),
+              const EdgeInsets.symmetric(vertical: 32, horizontal: 48.0),
+              child: Observer(builder: (_) {
+                return Wrap(
+                  spacing: 24,
+                  children: ReefAppState
+                      .instance.model.tokens.selectedSignerNFTs
+                      .map((TokenNFT tkn) {
+                    return Column(
+                      children: [
+                        tokenCard(tkn.name,
+                            tokenName: tkn.symbol,
+                            iconURL:  tkn.iconUrl,
+                            price: tkn.price?.toDouble()??0,
+                            balance: decimalsToDouble(tkn.balance)
+                        ),
+                      ],
+                    );
+                  }).toList(),
+                );
+              }),
             ),
           )
         ]);
