526741d5857db6d66de3e4dbfa67b98f88fd1aa0
Matjaz Hirschman <matjazonline@gmail.com>
2022-11-13 22:03:37 +0100

    Load NFTs (#53)
    * nft card
    
    * nft card
    
    * remove staking

--- lib/components/home/NFT_view.dart
+++ lib/components/home/NFT_view.dart
@@ -1,6 +1,12 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:gap/gap.dart';
+import 'package:reef_mobile_app/model/tokens/TokenNFT.dart';
 import 'package:reef_mobile_app/utils/elements.dart';
 import 'package:reef_mobile_app/utils/styles.dart';
+
+import '../../model/ReefAppState.dart';
 
 class NFTView extends StatefulWidget {
   const NFTView({Key? key}) : super(key: key);
@@ -10,6 +16,38 @@ class NFTView extends StatefulWidget {
 }
 
 class _NFTViewState extends State<NFTView> {
+  Widget nftCard(String name, String iconURL, int balance) {
+    return ViewBoxContainer(
+        imageUrl: iconURL,
+        child:  Column(
+           mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Container(
+                    color: Colors.white,
+                    child: Row(children: [
+                      const Gap(24),
+                      Text(
+                        // TODO allow conversionRate to be null for no data
+                        "${balance}x",
+                        style: TextStyle(
+                            color: Styles.textLightColor, fontSize: 16),
+                      ),
+                      const Gap(8),
+                      Text(
+                        name,
+                        style: TextStyle(
+                          color: Styles.textColor,
+                          fontSize: 20,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                    ])),
+                const Gap(8),
+              ],
+            )
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return ListView(
@@ -17,28 +55,25 @@ class _NFTViewState extends State<NFTView> {
         padding: const EdgeInsets.all(0),
         children: [
           SizedBox(
-            // constraints: const BoxConstraints.expand(),
             width: double.infinity,
-            // // replace later, just for debugging
-            // decoration: BoxDecoration(
-            //   border: Border.all(
-            //     color: Colors.red,
-            //   ),
-            // ),
             child: Padding(
               padding:
                   const EdgeInsets.symmetric(vertical: 32, horizontal: 48.0),
-              child: ViewBoxContainer(
-                  child: Padding(
-                padding: const EdgeInsets.symmetric(vertical: 24.0),
-                child: Column(
-                  children: [
-                    Text("No NFTs in your wallet",
-                        style: TextStyle(
-                            color: Styles.textLightColor, fontSize: 16)),
-                  ],
-                ),
-              )),
+              child: Observer(builder: (_) {
+                return Wrap(
+                  runSpacing: 24,
+                  children: ReefAppState
+                      .instance.model.tokens.selectedSignerNFTs
+                      .map((TokenNFT tkn) {
+                    return Column(
+                      children: [
+                        nftCard(tkn.name, tkn.iconUrl ?? '',
+                            tkn.balance.toInt() ?? 0),
+                      ],
+                    );
+                  }).toList(),
+                );
+              }),
             ),
           )
         ]);
