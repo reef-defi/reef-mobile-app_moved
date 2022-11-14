5749ff7cc0302539dbdd74c8341f89e6742609c2
Matjaz Hirschman <matjazonline@gmail.com>
2022-11-14 22:41:09 +0100

    nft UI (#58)

--- lib/components/home/NFT_view.dart
+++ lib/components/home/NFT_view.dart
@@ -19,33 +19,74 @@ class _NFTViewState extends State<NFTView> {
   Widget nftCard(String name, String iconURL, int balance) {
     return ViewBoxContainer(
         imageUrl: iconURL,
-        child:  Column(
-           mainAxisAlignment: MainAxisAlignment.end,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Row(
+              mainAxisSize: MainAxisSize.min,
               children: [
                 Container(
-                    color: Colors.white,
+                    decoration: BoxDecoration(
+                        color: Colors.purple,
+                        borderRadius: BorderRadius.all(Radius.circular(20))),
+                    child: Row(children: [
+                      const Gap(12),
+                      Text(
+                        // TODO allow conversionRate to be null for no data
+                        "${balance}x",
+                        style: TextStyle(
+                            color: Styles.textLightColor,
+                            fontSize: 16,
+                            overflow: TextOverflow.ellipsis),
+                      ),
+                      const Gap(8),
+                      Text(
+                        name,
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                            fontWeight: FontWeight.w700,
+                            overflow: TextOverflow.ellipsis),
+                      ),
+
+                      const Gap(12),
+                    ]))
+              ],
+            ),
+            const Gap(12)
+          ],
+        )
+        /*Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Container(
+                    decoration: BoxDecoration(
+                        color: Colors.purple,
+                        borderRadius: BorderRadius.all(Radius.circular(20))),
                     child: Row(children: [
                       const Gap(24),
                       Text(
                         // TODO allow conversionRate to be null for no data
                         "${balance}x",
                         style: TextStyle(
-                            color: Styles.textLightColor, fontSize: 16),
+                            color: Styles.textLightColor,
+                            fontSize: 16,
+                            overflow: TextOverflow.ellipsis),
                       ),
                       const Gap(8),
                       Text(
                         name,
                         style: TextStyle(
-                          color: Styles.textColor,
-                          fontSize: 20,
-                          fontWeight: FontWeight.w700,
-                        ),
+                            color: Colors.white,
+                            fontSize: 20,
+                            fontWeight: FontWeight.w700,
+                            overflow: TextOverflow.ellipsis),
                       ),
                     ])),
                 const Gap(8),
               ],
-            )
-    );
+            )*/
+        );
   }
 
   @override
