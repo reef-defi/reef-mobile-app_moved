1e11c3b72b7341e0108a4670f35b3733f1c72ca6
matjazonline <matjazonline@gmail.com>
2022-11-14 23:31:33 +0100

    fix when json balance not bigint

--- lib/utils/json_big_int.dart
+++ lib/utils/json_big_int.dart
@@ -1,20 +1,27 @@
 class JsonBigInt {
   final String type;
-  final String hex;
-  JsonBigInt(this.type, this.hex);
+  final String value;
 
-  BigInt? _toBigInt (){
-    if(type == "BigNumber") {
-      return BigInt.tryParse(hex.substring(2), radix: 16);
+  JsonBigInt(this.type, this.value);
+
+  BigInt? _toBigInt() {
+    if (type == "BigNumber") {
+      return BigInt.tryParse(value.substring(2), radix: 16);
+    }
+    if (type == 'Int') {
+      return BigInt.tryParse(value, radix: 10);
     }
     return null;
   }
 
-  static JsonBigInt _fromJson(dynamic jsonVal){
+  static JsonBigInt _fromJson(dynamic jsonVal) {
+    if (jsonVal is int) {
+      return JsonBigInt('Int', jsonVal.toString());
+    }
     return JsonBigInt(jsonVal['type'], jsonVal['hex']);
   }
 
-  static BigInt? toBigInt(dynamic jsonVal){
-    return jsonVal!=null ? _fromJson(jsonVal)._toBigInt() : null;
+  static BigInt? toBigInt(dynamic jsonVal) {
+    return jsonVal != null ? _fromJson(jsonVal)._toBigInt() : null;
   }
 }
