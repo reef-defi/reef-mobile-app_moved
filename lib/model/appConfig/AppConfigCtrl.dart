e10e14282d770f90ecd3407e6413f0ce4a2ed5fa
matjazonline <matjazonline@gmail.com>
2023-01-03 01:10:46 +0100

    blur component wireframe - check ...TODO

---
+++ lib/model/appConfig/AppConfigCtrl.dart
@@ -0,0 +1,10 @@
+class AppConfig {
+
+    AppConfig(this._storage, this._appConfigModel){
+        ...TODO set model value from storage
+    }
+
+    toggle(){
+        ...TODO toggle value in storage, set model value from storage
+    }
+}
