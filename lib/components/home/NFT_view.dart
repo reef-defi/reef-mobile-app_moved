6fa76185c38e87100c7bd875137d9dad8a79bb2a
matjazonline <matjazonline@gmail.com>
2023-02-19 08:56:18 +0100

    cleanup

--- lib/components/home/NFT_view.dart
+++ lib/components/home/NFT_view.dart
@@ -62,30 +62,6 @@ class _NFTViewState extends State<NFTView> {
                         const Gap(8),
                       ],
                     ))
-
-                /*Observer(builder: (context) {
-                    return BlurableContent(
-                        Flexible(
-                            child: Text(
-                          // TODO allow conversionRate to be null for no data
-                          "${NumberFormat.compact().format(balance)}",
-                          style: TextStyle(
-                              color: Styles.textLightColor,
-                              fontSize: 16,
-                              overflow: TextOverflow.ellipsis),
-                        )),
-                        ReefAppState.instance.model.appConfig.displayBalance);
-                  }),
-                  const Gap(8),
-                  Text(
-                    name,
-                    style: const TextStyle(
-                        color: Colors.white,
-                        fontSize: 20,
-                        fontWeight: FontWeight.w700,
-                        overflow: TextOverflow.ellipsis),
-                  ),
-                  const Gap(12),*/
               ],
             ),
           ],
@@ -144,47 +120,6 @@ class _NFTViewState extends State<NFTView> {
             ),
           ),
       ]);
-
-      // return Flex(direction: Axis.vertical, children: [
-      //   if (message != null)
-      //     Padding(
-      //       padding: const EdgeInsets.symmetric(vertical: 32),
-      //       child: ViewBoxContainer(
-      //           child: Center(
-      //               child: Padding(
-      //         padding: const EdgeInsets.symmetric(vertical: 24.0),
-      //         child: Text(
-      //           message,
-      //           style: TextStyle(
-      //               color: Styles.textLightColor, fontWeight: FontWeight.w500),
-      //         ),
-      //       ))),
-      //     )
-      //   else
-      //     Expanded(
-      //         child: GridView.builder(
-      //       controller: ScrollController(),
-      //       scrollDirection: Axis.vertical,
-      //       gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-      //           mainAxisExtent: 125,
-      //           mainAxisSpacing: 24,
-      //           crossAxisSpacing: 24,
-      //           maxCrossAxisExtent: 200),
-      //       itemBuilder: (context, index) => Builder(
-      //         builder: (context) {
-      //           final tkn =
-      //               ReefAppState.instance.model.tokens.selectedNFTs.data[index];
-      //           return nftCard(
-      //             tkn.data.name,
-      //             tkn.data.iconUrl ?? '',
-      //             tkn.data.balance.toInt(),
-      //           );
-      //         },
-      //       ),
-      //       itemCount:
-      //           ReefAppState.instance.model.tokens.selectedNFTs.data.length,
-      //     ))
-      // ]);
     });
   }
 }
