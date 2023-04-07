259af2842a861cc57d32bcd7af9798b563256b06
Anukul Pandey <anukul.030601@gmail.com>
2023-04-07 18:43:30 +0530

    feat: account iconSvg

--- lib/pages/accounts_page.dart
+++ lib/pages/accounts_page.dart
@@ -18,6 +18,7 @@ import 'package:reef_mobile_app/model/account/AccountCtrl.dart';
 import 'package:reef_mobile_app/model/account/stored_account.dart';
 import 'package:reef_mobile_app/model/navigation/navigation_model.dart';
 import 'package:reef_mobile_app/model/status-data-object/StatusDataObject.dart';
+import 'package:reef_mobile_app/utils/account_profile.dart';
 import 'package:reef_mobile_app/utils/styles.dart';
 
 import '../components/sign/SignatureContentToggle.dart';
@@ -32,28 +33,7 @@ class AccountsPage extends StatefulWidget {
 }
 
 class _AccountsPageState extends State<AccountsPage> {
-  final svgData = """
-<svg viewBox='0 0 64 64' xmlns='http://www.w3.org/2000/svg'>
-  <circle cx='32' cy='32' fill='#eee' r='32' />
-  <circle cx='32' cy='8' fill='hsl(45, 40%, 35%)' r='5' />
-  <circle cx='32' cy='20' fill='hsl(354, 40%, 35%)' r='5' />
-  <circle cx='21.607695154586736' cy='14' fill='hsl(168, 40%, 35%)' r='5' />
-  <circle cx='11.215390309173472' cy='20' fill='hsl(106, 40%, 15%)' r='5' />
-  <circle cx='21.607695154586736' cy='26' fill='hsl(331, 40%, 15%)' r='5' />
-  <circle cx='11.215390309173472' cy='32' fill='hsl(5, 40%, 53%)' r='5' />
-  <circle cx='11.215390309173472' cy='44' fill='hsl(45, 40%, 35%)' r='5' />
-  <circle cx='21.607695154586736' cy='38' fill='hsl(354, 40%, 35%)' r='5' />
-  <circle cx='21.607695154586736' cy='50' fill='hsl(168, 40%, 35%)' r='5' />
-  <circle cx='32' cy='56' fill='hsl(106, 40%, 15%)' r='5' />
-  <circle cx='32' cy='44' fill='hsl(331, 40%, 15%)' r='5' />
-  <circle cx='42.392304845413264' cy='50' fill='hsl(5, 40%, 53%)' r='5' />
-  <circle cx='52.78460969082653' cy='44' fill='hsl(45, 40%, 35%)' r='5' />
-  <circle cx='42.392304845413264' cy='38' fill='hsl(354, 40%, 35%)' r='5' />
-  <circle cx='52.78460969082653' cy='32' fill='hsl(168, 40%, 35%)' r='5' />
-  <circle cx='42.392304845413264' cy='26' fill='hsl(106, 40%, 15%)' r='5' />
-  <circle cx='52.78460969082653' cy='20' fill='hsl(331, 40%, 15%)' r='5' />
-</svg>
-""";
+  final svgData = AccountProfile.iconSvg;
   // TODO replace strings with enum
   void openModal(String modalName) {
     switch (modalName) {
