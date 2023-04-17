ca2206221e570404f7b953e7fe9002991dc41598
Anukul Pandey <anukul.030601@gmail.com>
2023-04-17 15:17:48 +0530

    fix: accountJsonData splitting function

--- lib/js/packages/reef-mobile-js/src/jsApi/signing/signer.ts
+++ lib/js/packages/reef-mobile-js/src/jsApi/signing/signer.ts
@@ -6,35 +6,62 @@ import { KeyringPair, KeyringPair$Json } from '@polkadot/keyring/types';
 import type { SignerPayloadJSON } from '@polkadot/types/types';
 
 async function signPayload(mnemonic: string, payload: SignerPayloadJSON): Promise<string> {
-    const registry = new TypeRegistry();
-    registry.setSignedExtensions(payload.signedExtensions);
-    var pair:KeyringPair;
-    //if the mnemonic phrase consists of plus it means it is not the normal mnemonic instead we are using this to get the keyring with which we will sign the tx
-    
-    // we can not sign the tx with private/secret key as there isn't any such function
+  const registry = new TypeRegistry();
+  registry.setSignedExtensions(payload.signedExtensions);
+  var pair: KeyringPair;
+  //if the mnemonic phrase consists of plus it means it is not the normal mnemonic instead we are using this to get the keyring with which we will sign the tx
 
-    // IF ANY OF THE STEPS IS WRONG THEN THE TX WILL BE FAILED : INVALID
-    if(mnemonic.indexOf('+')!=-1){
-      // fetching the params for getting account.json file 
-      const accountJsonDataParams=mnemonic.split('+');
+  // we can not sign the tx with private/secret key as there isn't any such function
 
-      const _address = accountJsonDataParams[0]; // address of account
-      const _password = accountJsonDataParams[1]; // password to decrypt account.json
+  // IF ANY OF THE STEPS IS WRONG THEN THE TX WILL BE FAILED : INVALID
+  if (mnemonic.indexOf('+') != -1) {
+    // fetching the params for getting account.json file 
+    const delimiterIndex = mnemonic.indexOf('+');
+    const accountJsonDataParams = [
+      mnemonic.slice(0, delimiterIndex),
+      mnemonic.slice(delimiterIndex + 1)
+    ];
 
-      var accountJsonData = await Keyring.exportAccountQr(_address,_password); // getting account.json
-      pair = await Keyring.krFromJson(accountJsonData["exportedJson"]); // generating pair with exported json file
-      await pair.unlock(_password); // unlocking the pair for signing tx
-    }else{
-      // If the mnemonic doesn't have a "+" , it is normal mnemonic with which we will sign the tx
-      pair = await Keyring.keyPairFromMnemonic(mnemonic);
-    }
-    return registry
-        .createType('ExtrinsicPayload', payload, { version: payload.version })
-        .sign(pair).signature;
+    const _address = accountJsonDataParams[0]; // address of account
+    const _password = accountJsonDataParams[1]; // password to decrypt account.json
+
+    var accountJsonData = await Keyring.exportAccountQr(_address, _password); // getting account.json
+    pair = await Keyring.krFromJson(accountJsonData["exportedJson"]); // generating pair with exported json file
+    await pair.unlock(_password); // unlocking the pair for signing tx
+  } else {
+    // If the mnemonic doesn't have a "+" , it is normal mnemonic with which we will sign the tx
+    pair = await Keyring.keyPairFromMnemonic(mnemonic);
+  }
+  return registry
+    .createType('ExtrinsicPayload', payload, { version: payload.version })
+    .sign(pair).signature;
 }
 
-async function signRaw(mnemonic: any, message: string): Promise<string>  {
-  const pair: KeyringPair = await Keyring.keyPairFromMnemonic(mnemonic);
+async function signRaw(mnemonic: any, message: string): Promise<string> {
+  var pair: KeyringPair;
+  //if the mnemonic phrase consists of plus it means it is not the normal mnemonic instead we are using this to get the keyring with which we will sign the tx
+
+  // we can not sign the tx with private/secret key as there isn't any such function
+
+  // IF ANY OF THE STEPS IS WRONG THEN THE TX WILL BE FAILED : INVALID
+  if (mnemonic.indexOf('+') != -1) {
+    // fetching the params for getting account.json file 
+    const delimiterIndex = mnemonic.indexOf('+');
+    const accountJsonDataParams = [
+      mnemonic.slice(0, delimiterIndex),
+      mnemonic.slice(delimiterIndex + 1)
+    ];
+
+    const _address = accountJsonDataParams[0]; // address of account
+    const _password = accountJsonDataParams[1]; // password to decrypt account.json
+
+    var accountJsonData = await Keyring.exportAccountQr(_address, _password); // getting account.json
+    pair = await Keyring.krFromJson(accountJsonData["exportedJson"]); // generating pair with exported json file
+    await pair.unlock(_password); // unlocking the pair for signing tx
+  } else {
+    // If the mnemonic doesn't have a "+" , it is normal mnemonic with which we will sign the tx
+    pair = await Keyring.keyPairFromMnemonic(mnemonic);
+  }
 
   return u8aToHex(pair.sign(wrapBytes(message)));
 }
--- lib/utils/password_manager.dart
+++ lib/utils/password_manager.dart
@@ -5,38 +5,48 @@ import 'package:reef_mobile_app/model/StorageKey.dart';
 import 'package:reef_mobile_app/model/account/stored_account.dart';
 import 'package:reef_mobile_app/service/JsApiService.dart';
 
-class PasswordManager{
-  static Future<bool> checkIfPassword()async{
+class PasswordManager {
+  static Future<bool> checkIfPassword() async {
     try {
-    final password = await ReefAppState.instance.storageCtrl.getValue(StorageKey.password.name);
-    if(password==null)return false;
-    return true;
+      final password = await ReefAppState.instance.storageCtrl
+          .getValue(StorageKey.password.name);
+      if (password == null) return false;
+      return true;
     } catch (e) {
-    return false;
+      return false;
     }
   }
 
-  static void updateAccountPassword(StoredAccount account,String newPass)async{
-    final accountElems = account.mnemonic.split("+");
+  static void updateAccountPassword(
+      StoredAccount account, String newPass) async {
+    // to only split from the first occurence considering that password might contain '+' in it    
+    int delimiterIdx = account.mnemonic.indexOf("+");
+    List<String> accountElems = [
+      account.mnemonic.substring(0, delimiterIdx),
+      account.mnemonic.substring(delimiterIdx + 1),
+    ];
     String oldPass = accountElems[1];
-    Map<String,dynamic> updatedAccountObj = {
-        'name':account.name,
-        'svg':account.svg,
-        'address':account.address,
-        'mnemonic':"${account.address}+$newPass"
-       };
-       final updatedAccount = StoredAccount.fromString(jsonEncode(updatedAccountObj).toString());
-         ReefAppState.instance.storage.deleteAccount(account.address);
-       ReefAppState.instance.accountCtrl.saveAccount(updatedAccount);
-       final accountPassChanged =await ReefAppState.instance.accountCtrl.changeAccountPassword(account.address, newPass, oldPass);
-       print("CHANGING ACCOUNT PASSWORD FOR ${account.address} - CHANGED PASSWORD :  $accountPassChanged");
+    Map<String, dynamic> updatedAccountObj = {
+      'name': account.name,
+      'svg': account.svg,
+      'address': account.address,
+      'mnemonic': "${account.address}+$newPass"
+    };
+    final updatedAccount =
+        StoredAccount.fromString(jsonEncode(updatedAccountObj).toString());
+    ReefAppState.instance.storage.deleteAccount(account.address);
+    ReefAppState.instance.accountCtrl.saveAccount(updatedAccount);
+    final accountPassChanged = await ReefAppState.instance.accountCtrl
+        .changeAccountPassword(account.address, newPass, oldPass);
+    print(
+        "CHANGING ACCOUNT PASSWORD FOR ${account.address} - CHANGED PASSWORD :  $accountPassChanged");
   }
 
-  static void changePasswordForAll(String newPass)async{
+  static void changePasswordForAll(String newPass) async {
     final allAccounts = await ReefAppState.instance.storage.getAllAccounts();
-    for(StoredAccount account in allAccounts){
-      if(account.mnemonic.contains("+")){ 
-       updateAccountPassword(account, newPass);
+    for (StoredAccount account in allAccounts) {
+      if (account.mnemonic.contains("+")) {
+        updateAccountPassword(account, newPass);
       }
     }
   }
