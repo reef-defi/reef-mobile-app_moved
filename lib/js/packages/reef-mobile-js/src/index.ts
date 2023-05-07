90d5fb47abdb2ed07785bfd178738dd3ab944fba
Matjaz Hirschman <matjazonline@gmail.com>
2023-05-07 21:24:53 +0200

    Display versions (#169)

--- lib/js/packages/reef-mobile-js/src/index.ts
+++ lib/js/packages/reef-mobile-js/src/index.ts
@@ -1,6 +1,7 @@
 import polyfill from './polyfill';
 import {FlutterJS} from "flutter-js-bridge/src/FlutterJS";
 import {reefState, tokenUtil} from "@reef-chain/util-lib";
+import {version} from "../../../../js/node_modules/@reef-chain/util-lib/package.json";
 import {initFlutterApi} from "./jsApi/initFlutterApi";
 import Keyring from "./jsApi/keyring";
 import Signer from "./jsApi/signing/signer";
@@ -11,3 +12,4 @@ window['reefState'] = reefState;
 window['tokenUtil'] = tokenUtil;
 window['keyring'] = Keyring;
 window['signer'] = Signer;
+window['getReefJsVer'] = ()=>({reefAppJs:'0 .0.1', utilLib:version});
